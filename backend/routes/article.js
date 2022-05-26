61e1142887a21906c93ab921a2774b94d3e063b7
teddyrahsyah212 <teddyrahsyah@gmail.com>
2022-05-26 19:57:46 +0700

    create route article

--- backend/routes/article.js
+++ backend/routes/article.js
@@ -1,5 +1,16 @@
 import express from "express";
+
+import { upload } from "../middleware/uploadImage.js";
+import { verifyUser } from "../middleware/verifyToken.js";
+
+import { create, findAll, findOne, update, remove } from "../controllers/articleController.js";
+
 const router = express.Router();
 
+router.get("/", findAll);
+router.get("/:id", findOne);
+router.post("/create", [verifyUser, upload], create)
+router.put("/:id/update", [verifyUser, upload], update);
+router.delete("/:id", verifyUser, remove);
 
 export default router;
