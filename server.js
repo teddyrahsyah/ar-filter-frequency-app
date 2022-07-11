c6401e26d23e4faf757082aada1751fa407afa48
teddyrahsyah212 <teddyrahsyah@gmail.com>
2022-07-11 16:08:05 +0700

    coba

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ db.on("error", (err) => console.log(err));
 db.once("open", () => console.log("Arsim Database Connected..."));
 
 appServer.listen(process.env.APP_SERVER_PORT, () => {
-  console.log(`App Server is running at http://localhost:${process.env.APP_SERVER_PORT}`);
+  console.log(`App Server is running at http://localhost:${process.env.APP_SERVER_PORT || 8000}`);
 })
 
 authServer.listen(process.env.AUTH_SERVER_PORT, () => {
-  console.log(`Auth Server is running at http://localhost:${process.env.AUTH_SERVER_PORT}`);
+  console.log(`Auth Server is running at http://localhost:${process.env.AUTH_SERVER_PORT || 8010}`);
 });
