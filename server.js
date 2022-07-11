418c91747c6b499ed00ca254c212deb7111bf4ff
teddyrahsyah212 <teddyrahsyah@gmail.com>
2022-07-11 17:14:54 +0700

    restructure server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+import path from "path";
 import dotenv from "dotenv";
 import db from "./config/dbConfig.js";
-import appServer from "./appServer.js";
-import authServer from "./authServer.js";
+
+const app = express();
 
 // Environment Variable Configuration
 dotenv.config();
@@ -10,10 +11,30 @@ dotenv.config();
 db.on("error", (err) => console.log(err));
 db.once("open", () => console.log("Arsim Database Connected..."));
 
-appServer.listen(process.env.PORT, () => {
-  console.log(`App Server is running at http://localhost:${process.env.PORT || 5000}`);
-})
+// Import Routes
+import authRoute from "./routes/auth.js";
+import moduleRoute from "./routes/module.js";
+import articleRoute from "./routes/article.js";
+import messageRoute from "./routes/message.js";
 
-authServer.listen(process.env.AUTH_SERVER_PORT, () => {
-  console.log(`Auth Server is running at http://localhost:${process.env.AUTH_SERVER_PORT || 8010}`);
-});
+// Middleware
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.static("public/uploads"));
+
+// Route MiddleWare
+app.use("/api/user", authRoute);
+app.use("/api/module", moduleRoute);
+app.use("/api/article", articleRoute);
+app.use("/api/message", messageRoute);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', "build", "index.html"))
+  })
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
